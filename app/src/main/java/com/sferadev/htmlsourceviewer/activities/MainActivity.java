3a2734e369897aaf4bcdf41b8511da1f4aff0950
Alexis Rico <me@sferadev.com>
2015-10-02 20:14:48 +0200

    Version 1.2

--- app/src/main/java/com/sferadev/htmlsourceviewer/activities/MainActivity.java
+++ app/src/main/java/com/sferadev/htmlsourceviewer/activities/MainActivity.java
@@ -1,9 +1,13 @@
 package com.sferadev.htmlsourceviewer.activities;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -11,14 +15,19 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.sferadev.htmlsourceviewer.R;
 import com.sferadev.htmlsourceviewer.utils.NetworkUtils;
 import com.sferadev.htmlsourceviewer.utils.PreferenceUtils;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
+    private TextView textView;
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +39,25 @@ public class MainActivity extends AppCompatActivity {
 
         textView = (TextView) findViewById(R.id.textView);
 
-        loadCode();
+        handleIntent(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        setIntent(intent);
+        handleIntent(intent);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView = null;
+        if (searchItem != null) searchView = (SearchView) searchItem.getActionView();
+        if (searchView != null)
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -48,9 +70,28 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onBackPressed() {
+        if (!searchView.isIconified()) {
+            searchView.setIconified(true);
+            loadCode();
+        }
+    }
+
     private void loadCode() {
-        textView.setText(NetworkUtils.getURLOutput(PreferenceUtils.getPreference(this,
+        Document document = Jsoup.parse(NetworkUtils.getURLOutput(PreferenceUtils.getPreference(this,
                 PreferenceUtils.PROPERTY_LAST_WEB, "https://github.com/")));
+        textView.setText(document.outerHtml());
+    }
+
+    private void loadCode(String search) {
+        Document document = Jsoup.parse(NetworkUtils.getURLOutput(PreferenceUtils.getPreference(this,
+                PreferenceUtils.PROPERTY_LAST_WEB, "https://github.com/")));
+        if (document.select(search).hasText()) {
+            textView.setText(document.select(search).outerHtml());
+        } else {
+            Toast.makeText(this, getString(R.string.search_error), Toast.LENGTH_LONG).show();
+        }
     }
 
     private void showDialog() {
@@ -58,7 +99,7 @@ public class MainActivity extends AppCompatActivity {
         final View myDialogView = factory.inflate(R.layout.input_dialog, null);
         AlertDialog dialog = new AlertDialog.Builder(MainActivity.this,
                 android.R.style.Theme_DeviceDefault_Light_Dialog_NoActionBar_MinWidth)
-                .setTitle("RSS URL")
+                .setTitle("URL")
                 .setView(myDialogView)
                 .setPositiveButton(getString(android.R.string.yes), new DialogInterface.OnClickListener() {
                     @Override
@@ -74,6 +115,15 @@ public class MainActivity extends AppCompatActivity {
                 .setNegativeButton(getString(android.R.string.no), null)
                 .create();
         dialog.show();
+    }
+
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            loadCode(query);
+        } else {
+            loadCode();
+        }
     }
 
 }
